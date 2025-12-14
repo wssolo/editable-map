cfe09152be798ab03314dd1fd2c7ebb954bf6794
wssolo <102891403+wssolo@users.noreply.github.com>
2025-12-14 23:20:39 +0800

    Update map.js

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-const API_URL = "https://editable-map-api.workers.dev";
+const API_URL = "https://editable-map-api.ws530813759.workers.dev";
 
 let currentMapType = "amap";
 let map = null;
@@ -101,3 +101,4 @@ document.getElementById("btn-osm").onclick = () => {
    默认加载高德
 ====================== */
 initAMap();
+
